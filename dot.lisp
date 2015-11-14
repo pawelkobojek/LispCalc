311cc25c6303e1cf0f607139ecdbcfd51da3a41f
Paweł Kobojek <PawelKobojek@gmail.com>
2015-11-14 14:38:40 +0100

    symbolic dot product

---
+++ dot.lisp
@@ -0,0 +1,6 @@
+(defun dot (x y)
+  (if (not (equal (length x) (length y)))
+    NIL
+    (if (= (length x) 1)
+      `(* ,(first x) ,(first y))
+      `(+ (* ,(first x) ,(first y)) ,(dot (rest x) (rest y))))))
