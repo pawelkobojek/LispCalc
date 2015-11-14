43cfaab4a9a0f2b3a8f9bbe2e5e5e74e962c35f9
Paweł Kobojek <PawelKobojek@gmail.com>
2015-11-14 21:25:04 +0100

    dot product as macro

--- dot.lisp
+++ dot.lisp
@@ -4,3 +4,10 @@
     (if (= (length x) 1)
       `(* ,(first x) ,(first y))
       `(+ (* ,(first x) ,(first y)) ,(dot (rest x) (rest y))))))
+
+(defmacro dot-macro (x y)
+  `(if (not (equal (length ,x) (length ,y)))
+    NIL
+    (if (= (length ,x) 1)
+      `(* ,(first ,x) ,(first ,y))
+      `(+ (* ,(first ,x) ,(first ,y)) ,(dot (rest ,x) (rest ,y))))))
