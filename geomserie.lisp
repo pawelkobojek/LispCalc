f385cda188727a38a9e37699133163dbcba294d4
ED <ED>
2015-11-14 17:29:22 +0100

    working geomserie

--- geomserie.lisp
+++ geomserie.lisp
@@ -2,10 +2,22 @@
 	(
 		if (= n 0)
 			qn
-			(+ qn (int_geomserie (* qn q) q (- n 1)))
+			`(+ ,qn ,(int_geomserie `(* ,qn ,q) q (- n 1)))
 	)
 )
 
 (defun geomserie (q n)
 	(int_geomserie 1 q n)
+)
+
+(defmacro macroint_geomserie (qn q n)
+	(
+		if (= n 0)
+			qn
+			`(list '+ ,qn (macroint_geomserie ,`(list '* ,qn ,q) ,q ,(- n 1)))
+	)
+)
+
+(defmacro macrogeomserie (q n)
+	`(macroint_geomserie 1 ,q ,n)
 )
