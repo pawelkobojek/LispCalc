fb960d9cced6d7e9b51c4b51918c8a939cef093d
ED <ED>
2015-11-14 13:51:52 +0100

    available to set precision dynamically

--- exponent.lisp
+++ exponent.lisp
@@ -1,16 +1,14 @@
-(setq exponent_precision 10)
-
-(defun int_exponent (xn sn x n)
+(defun int_exponent (xn sn x n exponent_precision)
 	(
 		if (= n exponent_precision) 
 			`(/ ,xn ,sn)
 			(
 				if (= n 0)
-					`(+ 1 ,(int_exponent `(* ,xn ,x) (* sn (+ n 1)) x (+ n 1)))
-					`(+ (/ ,xn, sn) ,(int_exponent `(* ,xn ,x) (* sn (+ n 1)) x (+ n 1)))
+					`(+ 1 ,(int_exponent `(* ,xn ,x) (* sn (+ n 1)) x (+ n 1) exponent_precision))
+					`(+ (/ ,xn, sn) ,(int_exponent `(* ,xn ,x) (* sn (+ n 1)) x (+ n 1) exponent_precision))
 			)
 	)
 )
 
-(defun exponent (x)
-	`(float ,(int_exponent x 1 x 0)))
+(defun exponent (x n)
+	`(float ,(int_exponent x 1 x 0 n)))
