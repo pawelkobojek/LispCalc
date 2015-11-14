15ff7dbe490a52806142466ed28551ed5a7fcc77
ED <ED>
2015-11-14 15:53:08 +0100

    macro exponent works

--- exponent.lisp
+++ exponent.lisp
@@ -1,6 +1,6 @@
 (defun int_exponent (xn sn x n exponent_precision)
 	(
-		if (= n exponent_precision) 
+		if (= n exponent_precision)
 			`(/ ,xn ,sn)
 			(
 				if (= n 0)
@@ -12,3 +12,19 @@
 
 (defun exponent (x n)
 	`(float ,(int_exponent x 1 x 0 n)))
+
+(defmacro macroint_exponent (xn sn x n exponent_precision)
+	(
+		if (= n exponent_precision)
+			`(list '/ ,xn ,sn)
+			(
+				if (= n 0)
+					`(list '+ 1 (macroint_exponent ,`(list '* ,xn ,x) ,(* sn (+ n 1)) ,x ,(+ n 1) ,exponent_precision))
+					`(list '+ ,`(list '/ ,xn ,sn) (macroint_exponent ,`(list '* ,xn ,x) ,(* sn (+ n 1)) ,x ,(+ n 1) ,exponent_precision))
+			)
+	)
+)
+
+(defmacro macroexponent (x n)
+	`(list 'float (macroint_exponent ,x 1 ,x 0 ,n))
+)
